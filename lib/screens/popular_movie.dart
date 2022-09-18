6a9fb8a7a045a1bbe1c31fd98044da680d132d81
innocenty-lab <74968146+innocenty-lab@users.noreply.github.com>
2022-09-18 21:03:11 +0700

    infinite list pagination

--- lib/screens/popular_movie.dart
+++ lib/screens/popular_movie.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:movie_test_app/screens/movie_detail.dart';
@@ -12,14 +13,7 @@ class PopularMoviePage extends StatefulWidget {
 }
 
 class _PopularMoviePageState extends State<PopularMoviePage> {
-  List<Results> results = [];
-  ApiService apiService = ApiService();
   String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-
-  Future<List<Results>> getResults() async {
-    results = await apiService.getPopularMovies(1);
-    return results;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,90 +22,146 @@ class _PopularMoviePageState extends State<PopularMoviePage> {
         title: Text("Popular Movie"),
         centerTitle: true,
       ),
-      body: FutureBuilder<List<Results>>(
-        future: getResults(),
+      body: FutureBuilder(
+        future: ApiService().getPopularMovies(1),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
+          if (snapshot.data == null) {
+            return const Center(
               child: CircularProgressIndicator(),
             );
           } else {
-            print(snapshot.data);
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context, 
-                      MaterialPageRoute(
-                        builder: (_) => MovieDetailPage(),
-                        settings: RouteSettings(
-                          arguments: snapshot.data?[index],
-                        )
-                      )).then((_) {
-                        setState(() {
-                          getResults();
-                        });
-                      });
-                  },
-                  child: Card(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
+            return PopularMovieList(imageBaseUrl: imageBaseUrl, popularMovie: snapshot.data as dynamic);
+          }
+        },
+      )
+    );
+  }
+}
+
+class PopularMovieList extends StatefulWidget {
+  final PopularMovies popularMovie;
+  final String imageBaseUrl;
+
+  const PopularMovieList({
+    Key? key,
+    required this.imageBaseUrl, 
+    required this.popularMovie,
+  }) : super(key: key);
+
+  @override
+  State<PopularMovieList> createState() => _PopularMoviesState();
+}
+
+class _PopularMoviesState extends State<PopularMovieList> {
+  ScrollController scrollController = ScrollController();
+  List<Results> popularMovie = [];
+  int currentPage = 1;
+
+  bool onNotification(ScrollNotification notification) {
+    if (notification is ScrollUpdateNotification) {
+      if (scrollController.position.maxScrollExtent > scrollController.offset &&
+          scrollController.position.maxScrollExtent - scrollController.offset <=
+              50) {
+        print('End Scroll');
+        ApiService().getPopularMovies(currentPage + 1).then((val) {
+          currentPage = val.page;
+          setState(() {
+            popularMovie.addAll(val.results);
+          });
+        });
+      }
+    }
+    return true;
+  }
+
+  @override
+  void initState() {
+    popularMovie = widget.popularMovie.results;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener(
+      onNotification: onNotification,
+      child: ListView.builder(
+        itemCount: popularMovie.length,
+        controller: scrollController,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (_) => MovieDetailPage(),
+                  settings: RouteSettings(
+                    arguments: popularMovie[index],
+                  )
+                )).then((_) {
+                  setState(() {
+                    // getResults();
+                  });
+                });
+            },
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                height: 150,
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network("${widget.imageBaseUrl}${popularMovie[index].posterPath}"),
                     ),
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      height: 150,
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network("$imageBaseUrl${snapshot.data?[index].posterPath ?? snapshot.data?[0].posterPath}"),
-                          ),
-                          Expanded(
-                            child: Container(
-                              padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                              // color: Colors.red,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 6),
-                                    child: Text(
-                                      snapshot.data![index].title, 
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold, 
-                                        fontSize: 22,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 3.0),
-                                    child: Text(DateFormat("dd-MM-yyyy").format(snapshot.data![index].releaseDate)),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 3.0),
-                                    child: Text("Popularity : ${snapshot.data![index].popularity}"),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 3.0),
-                                    child: Text("Vote Average : ${snapshot.data![index].voteAverage}"),
-                                  ),
-                                ],
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                        // color: Colors.red,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 6),
+                              child: Text(
+                                popularMovie[index].title, 
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold, 
+                                  fontSize: 22,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
                               ),
                             ),
-                          )
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 3.0),
+                              child: Text(DateFormat("dd-MM-yyyy").format(popularMovie[index].releaseDate)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 3.0),
+                              child: Text("Popularity : ${popularMovie[index].popularity}"),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 3.0),
+                              child: Text("Vote Average : ${popularMovie[index].voteAverage}"),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              }
-            );
-          }
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
         }
       ),
     );
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -4,26 +4,14 @@ import 'package:http/http.dart' as http;
 import 'package:movie_test_app/model/popular_movies_model.dart';
 
 class ApiService {
-  // final _baseUrl = "https://api.themoviedb.org/3/movie/popular?api_key=";
-  // final _apiKey = "1ff37996775b3582b9ac66820e8c6417";
+  final _baseUrl = "https://api.themoviedb.org/3/movie/popular?api_key=";
+  final _apiKey = "1ff37996775b3582b9ac66820e8c6417";
 
-  // Future<List<Result>> getPopularMovies() async {
-  //   final response = await http.get(Uri.parse(_baseUrl+_apiKey));
-  //   // print(response.body);
-  //   if (response.statusCode == 200) {
-  //     return PopularMovies.fromJson(json: json.decode(response.body)).results;
-
-  //     // var jsonResonse =  PopularMovies.fromJson(json: json.decode(response.body)).results.toList();
-  //   } else {
-  //     throw Exception("Failed to get popular movie");
-  //   }
-  // }
-
-  Future<List<Results>> getPopularMovies(int currentPage) async {
-    var url = "https://api.themoviedb.org/3/movie/popular?api_key=1ff37996775b3582b9ac66820e8c6417&page=" + currentPage.toString();
+  Future<PopularMovies> getPopularMovies(int currentPage) async {
+    var url = "$_baseUrl$_apiKey&page=$currentPage";
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      return PopularMovies.fromJson(json: json.decode(response.body)).results;
+      return PopularMovies.fromJson(json: json.decode(response.body));
     } else {
       throw Exception("Failed to get popular movie");
     }
